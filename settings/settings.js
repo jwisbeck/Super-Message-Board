2acfd8282c035cabd51a78572192125ae092e32c
Elijah C <elijahchan21@gmail.com>
2019-04-07 17:02:45 -0400

    Pushing settings

--- settings/settings.js
+++ settings/settings.js
@@ -1,3 +1,42 @@
+/* Example for using file-system is at the bottom */
+
+function setup() {
+
+    modalSetup();
+
+
+}
+
+// Setup opening a modal
+function modalSetup() {
+    let listElements = document.getElementsByTagName('li');
+    for (let index = 0; index < listElements.length; index++) {
+        let li = listElements[index];
+        li.addEventListener('click', function () {
+            let name = this.id;
+            let modal = document.querySelector(`#${name}-modal`)
+            modal.style.display = 'block';
+        })
+    }
+
+    //Setup the close button
+    let closeBtns = document.querySelectorAll('.close');
+
+    for (let index = 0; index < closeBtns.length; index++) {
+        let btn = closeBtns[index];
+        btn.addEventListener('click', function () {
+            let currentModal = document.querySelectorAll('.modal');
+            currentModal[index].style.display = 'none';
+        })
+    }
+
+
+}
+
+setup();
+
+
+/* file-system example */
 let fs = require('file-system')
 
 let test = {
