c18495e1fabbd31840ce5b9f018ffbc23cabc87a
ahuang10 <35822503+ahuang10@users.noreply.github.com>
2019-04-16 14:05:15 -0400

    Added my version labeled 434Settings - Andy Version V2 (#7)
    * added my version of the settings for availability
    
    * This version corrects the added times to the HTML days

--- index.js
+++ index.js
@@ -6,14 +6,31 @@ const port = 3000;
 
 //Home directory
 
+<<<<<<< HEAD
+//Loads folder containing assets 
+app.use(express.static(__dirname + '/settings'));
+app.use(express.static(__dirname + '/queue'));
+app.use(express.static(__dirname + '/node_modules/fs-extra/'));
+
+
+
+//File that will display on startup
+app.get('/ele', function(req,res){
+  res.sendFile(__dirname + '/settings/settings.html')
+=======
 //Loads folder containing assets
 app.use(express.static(__dirname + '/settings'));
 
 //File that will display on startup
 app.get('/', function(req,res){
   res.sendFile(__dirname + '/queue/queueform.html')
+>>>>>>> c5c84ef85b2ea83a46a4f7a6f88564557e0e3970
 })
 
 app.listen(port, () =>{
   console.log(`Listening on port ${port}`)
+<<<<<<< HEAD
 });
+=======
+});
+>>>>>>> c5c84ef85b2ea83a46a4f7a6f88564557e0e3970
--- queue/queueform.js
+++ queue/queueform.js
@@ -1,4 +1,11 @@
 
+<<<<<<< HEAD
+
+//shift
+var classes=[];
+var currentpos=0;
+var last =currentpos+1;
+=======
   //var fs = require("fs");
 //shift
 var classes=[];
@@ -10,6 +17,7 @@ var queuefilname="../user_data/queue.txt";
 
 var currentpos=0;
 var last =currentpos;
+>>>>>>> c5c84ef85b2ea83a46a4f7a6f88564557e0e3970
 function Student(name,position,email, phonenum,cls,msg){
   this.name=name;
   this.posistion=position;
@@ -26,6 +34,13 @@ function dropd(){
   var numofclasses=classes.length;
   var s="";
   for(i=0;i<numofclasses;i++){
+<<<<<<< HEAD
+    "<option>"
+  }
+
+}
+dropd();
+=======
     s+="<option value=\""+classes[i]+"\">"+classes[i]+"<\/option>";
   }
   document.getElementById("smeagol").innerHTML=s;
@@ -34,6 +49,7 @@ dropd();
 function loadHome(){
 location.href = "HomePage.html";
 }
+>>>>>>> c5c84ef85b2ea83a46a4f7a6f88564557e0e3970
 function addToWaitlist(){
   var name;//doc get element name
   var email;
@@ -41,28 +57,47 @@ function addToWaitlist(){
   var position;
   var cls;
   var msg;
+<<<<<<< HEAD
+
+  name=document.getElementById("name").value;
+  email=document.getElementById("email").value;
+  phonenum=document.getElementById("num").value;
+  cls=document.getElementById("cls").value;
+=======
   //read
   name=document.getElementById("name").value;
   email=document.getElementById("email").value;
   phonenum=document.getElementById("phone").value;
   cls=document.getElementById("smeagol").value;
+>>>>>>> c5c84ef85b2ea83a46a4f7a6f88564557e0e3970
   msg=document.getElementById("msg").value;
   position=last+1;
   last=position
   var s1= new Student(name, position,email,phonenum,cls,msg);
+<<<<<<< HEAD
+  waitlist.push(s1);
+  alert("you have been added");
+=======
   waitlist.enqueue(s1);
   //write
   alert("you have been added to the queue, your number is: "+position);
+>>>>>>> c5c84ef85b2ea83a46a4f7a6f88564557e0e3970
 }
 
 function removeFromWaitlist(){
   currentpos+=1;
+<<<<<<< HEAD
+  waitlist.pop();
+=======
   waitlist.dequeue();
+>>>>>>> c5c84ef85b2ea83a46a4f7a6f88564557e0e3970
 }
 function removeFromWaitlist(i){
   currentpos+=1;
   waitlist.splice(i,1);
 }
+<<<<<<< HEAD
+=======
 function writeToFile(filename, list){
 waitlist=[];
 var text = fs.readFileSync(filename);
@@ -76,3 +111,4 @@ for(i=textByLine.length-1;i>=0;i--){
 function readFromFile(filename, list){
 
 }
+>>>>>>> c5c84ef85b2ea83a46a4f7a6f88564557e0e3970
